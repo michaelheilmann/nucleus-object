1fac19b25f99a63bb1f0eb52e50df96dbce6e977
Michael Heilmann <michaelheilmann@primordialmachine.com>
2018-06-20 19:33:40 +0200

    Update Nucleus: Object
    - Fix unused variable warning.
    - Fix Nucleus_ObjectArrayEnumerator.
    - Improve unit tests.

--- test/Signals/src/Nucleus.Object.Test.Signals/A.h
+++ test/Signals/src/Nucleus.Object.Test.Signals/A.h
@@ -1,2 +1,30 @@
 // Copyright (c) 2018 Michael Heilmann
 #pragma once
+
+#include "Nucleus/Object/Object.h"
+#include "Nucleus/Object/Types.h"
+#include "Nucleus/Object/Signals.h"
+
+#define Export
+Nucleus_ClassTypeDeclaration(Export,
+                             "A",
+                             A,
+                             Nucleus_Object)
+#define A(p) ((A *)(p))
+#define A_CLASS(p) ((A_Class *)(p))
+
+struct A
+{
+    Nucleus_Object parent;
+}; // struct A
+
+struct A_Class
+{
+    Nucleus_Object_Class parent;
+}; // struct A_Class
+
+Nucleus_NonNull() Nucleus_Status
+A_construct
+    (
+        A *self
+    );
